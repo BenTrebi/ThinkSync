a739ca57abda6e25ea7962ae914685f337a29ce2
Ben Trebilcock <trebi018@umn.edu>
2023-09-07 14:28:40 -0500

    Added to the think component and page.

--- client/src/components/ThinkComponent.jsx
+++ client/src/components/ThinkComponent.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
 export default function ThinkComponent() {
+  
+  function addChoiceInput() {
+    console.log("hello")
+  }
 
   return (
     <>
-    <h1>Think Space</h1>
+    <form className='createBracket'>
+      <h2>Create a Bracket:</h2>
+      <h4>Title/Question:</h4>
+      <input className='titleInput' type= "text"></input>
+      <h5>Options:</h5>
+      <ul>
+      <li className='choiceInput'><input type= "text"></input></li>
+      <li className='choiceInput'><input type= "text"></input></li>
+      <li className='choiceInput'><input type= "text"></input></li>
+      </ul>
+      <button type='addChoices' className='add-button' onClick={ addChoiceInput }>
+        Add Choice
+      </button>
+    </form>
     </>
   )
 }
