3bc37a8e4ee54bf8c4440e4b41ade7ef2372c218
John Garrison <splendidonkey@gmail.com>
2023-09-07 11:04:10 -0500

    added pages and components. added mdbootstrap 5 css framework. started some code for Navbar component.

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+} from 'mdb-react-ui-kit';
+
+export default function Navbar() {
+  // https://mdbootstrap.com/docs/react/navigation/navbar/
+  
+  return (
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+ 
+      </MDBContainer>
+    </MDBNavbar>
+  )
+}
