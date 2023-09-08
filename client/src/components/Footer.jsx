77496acfb3645d205f71f410cfb3ea90e1e20e02
John Garrison <splendidonkey@gmail.com>
2023-09-08 10:49:09 -0500

    styling changes for footer, added github link. added favicon and icons for navbar and footer.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,10 +13,28 @@ export default function Footer() {
 
   return (
     <MDBFooter className='fixed-bottom bg-dark text-center text-white'>
-      <div className='footer text-center p-3'>
-        <a className='text-white' href='/'>
-          ThinkSync &#128161;
-        </a>
+
+      <div className='footer text-center p-1'>
+
+        <img 
+            src='./assets/logo.png'
+            height='30'
+            alt=''
+            loading='lazy'
+            className='logo'
+          />
+          
+          &nbsp;&nbsp;
+
+          <a className='text-white' href='/'>
+            ThinkSync
+          </a>
+          
+          &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+
+          <a className='footer-link' href='https://github.com/BenTrebi/ThinkSync'>
+            GitHub&nbsp;&nbsp;<MDBIcon fab icon="github" />
+          </a>
       </div>
     </MDBFooter>
   )
