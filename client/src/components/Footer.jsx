9238489d0eae354d94414c0e709f5b1aaadacc5f
John Garrison <splendidonkey@gmail.com>
2023-09-08 10:19:01 -0500

    fixed link

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
   return (
     <MDBFooter className='fixed-bottom bg-dark text-center text-white'>
       <div className='footer text-center p-3'>
-        <a className='text-white' href='https://mdbootstrap.com/'>
+        <a className='text-white' href='/'>
           ThinkSync &#128161;
         </a>
       </div>
