d91e65ff8b2d3ddb07689eddc84eec77250b21c8
Ben Trebilcock <trebi018@umn.edu>
2023-09-11 15:32:49 -0500

    Commit before push.

--- client/src/components/ThinkComponent.jsx
+++ client/src/components/ThinkComponent.jsx
@@ -25,8 +25,27 @@ export default function ThinkComponent() {
     newForm.splice(i,1)
     setFormVal(newForm)
   }
-
-
+  const formValidation=() =>  {
+    const data = [...formVal]
+    for (let index = 0; index < data.length; index++) {
+      // const element = array[index];
+      if(data[index].idea == "")  {
+        data[index].ideaCheck= "idea required"
+      } else if (data[index].idea.length > 1) {
+        data[index].ideaLengthCheck = "idea needs to be greater than 1"
+      }
+      else{
+        data[index].ideaCheck =""
+        data[index].ideaLengthCheck = ""
+      }
+    }
+    setFormVal(data)
+  }
+  const onSubmit=(e) => {
+    e.preventDefault();
+    console.log("submitData", formVal)
+    formValidation(formVal)
+  }
 
 
   return (
@@ -40,7 +59,7 @@ export default function ThinkComponent() {
             <MDBInput name='Title' style={{ marginTop:"3%", marginBottom:"3%" }} label='Title/Question' id='titleQuestion' type='text' />
             <div>
               {formVal.map((item, i) =>(
-            <MDBInputGroup style={{marginTop:"1%", marginBottom:"1%"}}>
+            <MDBInputGroup style={{marginTop:"1%", marginBottom:"1%"}} key={i}>
             <MDBInput name='idea' label='Idea'  type='text' />
             <MDBBtn className="btn btn-primary" type="button" id="button-addon1" onClick={deleteIdea}>
                Delete
@@ -67,7 +86,7 @@ export default function ThinkComponent() {
             </MDBBtn>
             </MDBInputGroup>
             <MDBBtn style={{marginTop:"2%", marginRight: "80%"}} className='addButton' onClick={addIdea}>Add Choice</MDBBtn>
-            <MDBBtn style={{marginTop:"2%"}} className='submitButton'>Submit</MDBBtn>
+            <MDBBtn style={{marginTop:"2%"}} className='submitButton' onClick={onSubmit}>Submit</MDBBtn>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
