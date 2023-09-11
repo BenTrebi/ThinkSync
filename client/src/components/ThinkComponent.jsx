fb9c881b3042fb2115f23cc8bf58278d2253ceda
Ben Trebilcock <trebi018@umn.edu>
2023-09-11 13:42:42 -0500

    Added functionality to add choice and delete choice buttons.

--- client/src/components/ThinkComponent.jsx
+++ client/src/components/ThinkComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 
+
 import {
   MDBContainer,
   MDBCard,
@@ -10,21 +11,23 @@ import {
   MDBCol,
   MDBRow,
   MDBInput,
-  MDBBtnGroup,
   MDBInputGroup
 } from 'mdb-react-ui-kit';
 
-const thinkData = {
-  titleQuestion: "",
-  choices: "",
-}
 
 export default function ThinkComponent() {
-  
-  
-  function addChoiceInput() {
-    console.log("hello")
+  const [formVal, setFormVal] = useState([{ idea: '',}])
+  const addIdea = () => {
+    setFormVal([...formVal, {idea: '',}])
   }
+  const deleteIdea=(i) => {
+    const newForm = [...formVal]
+    newForm.splice(i,1)
+    setFormVal(newForm)
+  }
+
+
+
 
   return (
     <>
@@ -35,32 +38,36 @@ export default function ThinkComponent() {
             <MDBCardBody>
             <MDBCardTitle style={{color:'black'}}>Create a Bracket:</MDBCardTitle>
             <MDBInput name='Title' style={{ marginTop:"3%", marginBottom:"3%" }} label='Title/Question' id='titleQuestion' type='text' />
+            <div>
+              {formVal.map((item, i) =>(
             <MDBInputGroup style={{marginTop:"1%", marginBottom:"1%"}}>
-            <MDBInput name='Choice' label='Choice' id='Choice' type='text' />
-            <MDBBtn class="btn btn-primary" type="button" id="button-addon1">
+            <MDBInput name='idea' label='Idea'  type='text' />
+            <MDBBtn className="btn btn-primary" type="button" id="button-addon1" onClick={deleteIdea}>
+               Delete
+            </MDBBtn>
+            </MDBInputGroup>
+            ))}
+            </div>
+            <MDBInputGroup style={{marginTop:"1%", marginBottom:"1%"}}>
+            <MDBInput name='idea' label='Idea' type='text' />
+            <MDBBtn className="btn btn-primary" type="button" id="button-addon2" onClick={deleteIdea}>
                Delete
             </MDBBtn>
             </MDBInputGroup>
             <MDBInputGroup style={{marginTop:"1%", marginBottom:"1%"}}>
-            <MDBInput name='Choice' label='Choice' id='Choice' type='text' />
-            <MDBBtn class="btn btn-primary" type="button" id="button-addon1">
+            <MDBInput name='idea' label='Idea'  type='text' />
+            <MDBBtn className="btn btn-primary" type="button" id="button-addon3" onClick={deleteIdea}>
                Delete
             </MDBBtn>
             </MDBInputGroup>
             <MDBInputGroup style={{marginTop:"1%", marginBottom:"1%"}}>
-            <MDBInput name='Choice' label='Choice' id='Choice' type='text' />
-            <MDBBtn class="btn btn-primary" type="button" id="button-addon1">
-               Delete
+            <MDBInput name='idea' label='Idea' type='text' />
+            <MDBBtn className="btn btn-primary" type="button" id="button-addon4" onClick={deleteIdea}>
+              Delete
             </MDBBtn>
             </MDBInputGroup>
-            <MDBInputGroup style={{marginTop:"1%", marginBottom:"1%"}}>
-            <MDBInput name='Choice' label='Choice' id='Choice' type='text' />
-            <MDBBtn class="btn btn-primary" type="button" id="button-addon1">
-               Delete
-            </MDBBtn>
-            </MDBInputGroup>
-            <MDBBtn style={{marginTop:"2%", marginRight: "80%"}}>Add Choice</MDBBtn>
-            <MDBBtn style={{marginTop:"2%"}}>Submit</MDBBtn>
+            <MDBBtn style={{marginTop:"2%", marginRight: "80%"}} className='addButton' onClick={addIdea}>Add Choice</MDBBtn>
+            <MDBBtn style={{marginTop:"2%"}} className='submitButton'>Submit</MDBBtn>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
