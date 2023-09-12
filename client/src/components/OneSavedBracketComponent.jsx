0844d36ff7e50c6e91a84f9dbe58b4bfe72a4e34
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-09-12 13:39:42 -0500

    save before pull from main

--- client/src/components/OneSavedBracketComponent.jsx
+++ client/src/components/OneSavedBracketComponent.jsx
@@ -14,7 +14,7 @@ import {
 
 export default function OneSavedBracket() {
   //get current user from session
-  // const { currUser } = useUserContext()
+  const { currUser } = useUserContext()
 
 
   //put oneBracket into state
