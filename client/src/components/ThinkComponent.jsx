5b8157c7aa58998ef2586c599c8f6b1bedb2a33a
Ben Trebilcock <trebi018@umn.edu>
2023-09-12 09:30:18 -0500

    before pull.

--- client/src/components/ThinkComponent.jsx
+++ client/src/components/ThinkComponent.jsx
@@ -25,27 +25,32 @@ export default function ThinkComponent() {
     newForm.splice(i,1)
     setFormVal(newForm)
   }
-  const formValidation=() =>  {
-    const data = [...formVal]
-    for (let index = 0; index < data.length; index++) {
-      // const element = array[index];
-      if(data[index].idea == "")  {
-        data[index].ideaCheck= "idea required"
-      } else if (data[index].idea.length > 1) {
-        data[index].ideaLengthCheck = "idea needs to be greater than 1"
-      }
-      else{
-        data[index].ideaCheck =""
-        data[index].ideaLengthCheck = ""
-      }
-    }
-    setFormVal(data)
-  }
-  const onSubmit=(e) => {
-    e.preventDefault();
-    console.log("submitData", formVal)
-    formValidation(formVal)
-  }
+  // const onHandle = (e, i) =>  {
+  //   let newForm = [...formVal]
+  //   newForm[i][e.target.name]=e.target.value
+  //   setFormVal
+  // }
+  // const formValidation=() =>  {
+  //   const data = [...formVal]
+  //   for (let index = 0; index < data.length; index++) {
+  //     // const element = array[index];
+  //     if(data[index].idea == "")  {
+  //       data[index].ideaCheck= "idea required"
+  //     } else if (data[index].idea.length > 1) {
+  //       data[index].ideaLengthCheck = "idea needs to be greater than 1"
+  //     }
+  //     else{
+  //       data[index].ideaCheck =""
+  //       data[index].ideaLengthCheck = ""
+  //     }
+  //   }
+  //   setFormVal(data)
+  // }
+  // const onSubmit=(e) => {
+  //   e.preventDefault();
+  //   console.log("submitData", formVal)
+  //   formValidation(formVal)
+  // }
 
 
   return (
@@ -56,17 +61,15 @@ export default function ThinkComponent() {
           <MDBCard className='bg-dark'>
             <MDBCardBody>
             <MDBCardTitle style={{color:'black'}}>Create a Bracket:</MDBCardTitle>
-            <MDBInput name='Title' style={{ marginTop:"3%", marginBottom:"3%" }} label='Title/Question' id='titleQuestion' type='text' />
-            <div>
+            <MDBInput name='Title' style={{ marginTop:"3%", marginBottom:"3%" }} label='Title/Question' id='titleQuestion' type='text'/>
               {formVal.map((item, i) =>(
             <MDBInputGroup style={{marginTop:"1%", marginBottom:"1%"}} key={i}>
-            <MDBInput name='idea' label='Idea'  type='text' />
+            <MDBInput name='idea' label='Idea'  type='text'/>
             <MDBBtn className="btn btn-primary" type="button" id="button-addon1" onClick={deleteIdea}>
                Delete
             </MDBBtn>
             </MDBInputGroup>
             ))}
-            </div>
             <MDBInputGroup style={{marginTop:"1%", marginBottom:"1%"}}>
             <MDBInput name='idea' label='Idea' type='text' />
             <MDBBtn className="btn btn-primary" type="button" id="button-addon2" onClick={deleteIdea}>
@@ -86,7 +89,7 @@ export default function ThinkComponent() {
             </MDBBtn>
             </MDBInputGroup>
             <MDBBtn style={{marginTop:"2%", marginRight: "80%"}} className='addButton' onClick={addIdea}>Add Choice</MDBBtn>
-            <MDBBtn style={{marginTop:"2%"}} className='submitButton' onClick={onSubmit}>Submit</MDBBtn>
+            <MDBBtn style={{marginTop:"2%"}} className='submitButton' >Submit</MDBBtn>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
