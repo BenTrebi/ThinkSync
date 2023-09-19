5186461b93359fa82a5f5641ff7d8ce223cb5c74
John Garrison <splendidonkey@gmail.com>
2023-09-19 14:46:03 -0500

    implemented check for currUser cookie before making fetch call. removal of no longer needed console.log statements and commented out others. browser console clean for ready for future debugging.

--- client/src/components/SavedBrackets.jsx
+++ client/src/components/SavedBrackets.jsx
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { useUserContext } from "../utils/UserContext"
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useUserContext } from "../utils/UserContext";
+import { Link } from 'react-router-dom';
 import {
   MDBContainer,
   MDBCard,
   MDBCardBody,
   MDBCardTitle,
-  MDBCardText,
-  MDBCol,
-  MDBRow,
   MDBListGroup,
   MDBListGroupItem,
   MDBRipple,
@@ -16,51 +13,51 @@ import {
 } from 'mdb-react-ui-kit';
 
 export default function SavedBrackets() {
-
-  const { currUser } = useUserContext()
-  console.log(currUser)
-
-  const [ brackets, setBrackets ] = useState([])
+  const { currUser } = useUserContext();
+  const [brackets, setBrackets] = useState([]);
 
   async function getBrackets() {
-    const result = await fetch(`/api/bracket/history/${currUser.data._id}`)
-    const data = await result.json()
-    setBrackets(data)
+    try {
+      // Check to see if currUser cookie exists (!null) before fetching with currUser object.
+      if (currUser && currUser.data && currUser.data._id) {
+        const result = await fetch(`/api/bracket/history/${currUser.data._id}`);
+        const data = await result.json();
+        setBrackets(data);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
 
-    useEffect(() => {
-      getBrackets()
-  },[currUser])
-
+  useEffect(() => {
+    getBrackets();
+  }, [currUser]);
 
   return (
     <MDBContainer className='mt-5'>
-      <MDBRow>
-        <MDBCol col='2'>
-          <MDBCard className='bg-dark text-white'>
-            <MDBCardBody>
-            <MDBCardTitle className='text-white'>Your Completed Brackets:</MDBCardTitle>
-            <MDBListGroup className='mt-4'>
-
-              { brackets.map( (bracket) => (
-                <MDBRipple>
-                  
-                    <MDBListGroupItem action noBorders className='px-3 bg-dark'>
-                      <Link to ={`onesavedbracket/${bracket._id}`}>
-                        <MDBBtn className='saved-bracket-item'>
-                          {bracket.questionTitle}
-                        </MDBBtn>
-                      </Link>
-                    </MDBListGroupItem>
-
+      <MDBCard className='bg-dark text-white'>
+        <MDBCardBody>
+          <MDBCardTitle className='text-white'>Your Completed Brackets:</MDBCardTitle>
+          <MDBListGroup className='mt-4'>
+            {/* don't bother to render if the brackets array is empty */}
+            {brackets.length > 0 ? (
+              brackets.map((bracket) => (
+                <MDBRipple key={bracket._id}>
+                  <MDBListGroupItem action noBorders className='px-3 bg-dark'>
+                    <Link to={`onesavedbracket/${bracket._id}`}>
+                      <MDBBtn className='saved-bracket-item'>
+                        {bracket.questionTitle}
+                      </MDBBtn>
+                    </Link>
+                  </MDBListGroupItem>
                 </MDBRipple>
-              ))}
-
-            </MDBListGroup>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
+              ))
+            ) : (
+              <p>No saved brackets available.</p>
+            )}
+          </MDBListGroup>
+        </MDBCardBody>
+      </MDBCard>
     </MDBContainer>
-  )
+  );
 }
